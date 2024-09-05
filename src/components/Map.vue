4851fb73b778454cdedb5ec69521ae32e24b8286
LucaGiovannini02 <luca8giovannini@gmail.com>
2024-09-05 15:04:12 +0200

    fix marker map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LMarker, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
-import IconLocationSVG from '../assets/icon-location.svg'
-import { onMounted, ref, toRef } from "vue";
-import L, { DivIcon } from "leaflet";
+import { toRef } from "vue";
+import IconLocationSVG from '../assets/icon-location.svg';
+import { LIcon } from "@vue-leaflet/vue-leaflet";
 
 const props = defineProps({
     lat: { type: Number, required: true },
@@ -12,20 +12,6 @@ const props = defineProps({
 
 const lat = toRef(props, 'lat')
 const lon = toRef(props, 'lon')
-
-const svgIcon = ref<DivIcon | null>(null);
-
-onMounted(async () => {
-  const response = await fetch(IconLocationSVG);
-  const svgText = await response.text();
-
-  svgIcon.value = L.divIcon({
-    html: svgText,
-    className: "",
-    iconSize: [24, 40],
-    iconAnchor: [12, 40],
-  });
-});
 </script>
 
 <template>
@@ -33,7 +19,9 @@ onMounted(async () => {
         <l-map class="z-0" draggable="false" ref="map" :zoom=8 :center="[lat, lon]" :use-global-leaflet="false">
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
                 name="OpenStreetMap"></l-tile-layer>
-            <l-marker v-if="svgIcon" :icon="svgIcon" :draggable="false" :lat-lng="[lat, lon]" :visible="true" />
+            <l-marker :draggable="false" :lat-lng="[lat, lon]" :visible="true">
+                <l-icon :icon-url="IconLocationSVG" />
+            </l-marker>
         </l-map>
     </div>
 </template>
